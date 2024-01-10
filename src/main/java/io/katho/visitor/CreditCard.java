69b17788b3699be1ea9d196e248b634887f8f5ec
paulokatho <pkatho@gmail.com>
2024-01-10 16:18:54 +0000

    refactor: Movendo Interface Credit Card

--- src/main/java/io/katho/visitor/CreditCard.java
+++ src/main/java/io/katho/visitor/CreditCard.java
@@ -1,36 +1,44 @@
 package io.katho.visitor;
 
+
 public sealed abstract class CreditCard permits Visa, MasterCard {
     
     public abstract <T> T doSomething(CardVisitor<T> visitor);
-}
 
-/**
- * CardVisitor<T>
- */
-interface CardVisitor<T> {
     
-    public T visit(Visa visa);
-    
-    public T visit(MasterCard masterCard);
+    public static void main(String[] args) {
+        
+        CardVisitor<Integer> visitor = new CardVisitor<>() {            
+          
+            @Override
+            public Integer visit(MasterCard masterCard) {
+                return 2;
+            }
+            
+            @Override
+            public Integer visit(Visa visa) {
+                return 1;
+            }
+        };
 
-}
-
-final class Visa extends CreditCard {
-
-    @Override
-    public <T> T doSomething(CardVisitor<T> visitor) {        
-        return visitor.visit(this);
+            System.out.println("Imprimindo Visitor Visa: " + visitor.visit(new Visa()));
+        }
     }
-}
 
-final class MasterCard extends CreditCard {
-
-    @Override
-    public <T> T doSomething(CardVisitor<T> visitor) {
-        return visitor.visit(this);
+    final class MasterCard extends CreditCard {
+        @Override
+        public <T> T doSomething(CardVisitor<T> visitor) {
+            return visitor.visit(this);
+        }
     }
-}
+
+    final class Visa extends CreditCard {
+        @Override
+        public <T> T doSomething(CardVisitor<T> visitor) {        
+            return visitor.visit(this);
+        }
+
+    }
 
 
 
