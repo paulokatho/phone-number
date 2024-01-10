91702071a4bca2ad50e08bc32e6bc0c9d35a67f9
paulokatho <pkatho@gmail.com>
2024-01-10 15:20:55 +0000

    feat: Criar classe PatternMatching para utilizar com esse padrão de design de projetos. Esse modelo veio para substituir o Pattern Visitors que havia sido implantado no Java 8

---
+++ src/main/java/io/katho/visitor/PatternMatching.java
@@ -0,0 +1,34 @@
+package io.katho.visitor;
+
+/**
+ * Essa classe é para entendermos o Pattern Matching que veio para substituir o Patter Visitors
+ * Ele é após o Java 14
+ * 
+ * O Switch que estamos usando é um Switch Expression - A diferença para o Switch "normal" é que o normal não tem retorno
+ */
+public class PatternMatching {
+    
+    public int run(CreditCard card) {
+        int i = 5;
+        return switch (card) {
+            case Visa v -> 1;
+            case MasterCard m -> 2;
+        };
+    }
+}
+
+
+public class OtherPatternMatching {
+    
+    public String run(CreditCard card) {
+        int i = 5;
+        return switch (card) {
+            case Visa v -> "a";
+            case MasterCard m -> "b";
+        };
+    }
+}
+
+
+
+
